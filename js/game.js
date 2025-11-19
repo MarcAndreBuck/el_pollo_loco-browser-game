3dd3d82c44669d7f3ada47620df69e9c510ec1eb
Marc-Andre Buck <marcbuck94@gmail.com>
2025-11-19 13:04:10 +0100

    feat(keyboard): pass keyboard instance into world and prepare input handling

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@ init()
 
 function init() {
     canvas = document.getElementById("canvas")
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
 
     console.log("My Character is", world.character);
     
