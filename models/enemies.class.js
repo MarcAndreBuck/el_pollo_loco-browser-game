04da39bb698bafb26f0c38ebae1dd1292cb7058b
Marc-Andre Buck <marcbuck94@gmail.com>
2025-11-19 13:08:28 +0100

    refactor(chicken): migrate to ASSETS-based walk animation and update loop

--- models/enemies.class.js
+++ models/enemies.class.js
@@ -9,15 +9,19 @@ class Chicken extends MovableObject {
     constructor() {
         super();
 
+        this.animations = ASSETS.chicken_normal;
+
         this.speed = 0.2 + Math.random() * 0.4;
 
         this.x = 200 + Math.random() * 500;
+        this.y = 350;
 
-        this.loadImages(this.CHICKEN_NORMAL_WALK);
-        this.loadImage(this.CHICKEN_NORMAL_WALK[0]);
+        this.preloadAnimations(this.animations);
+        this.loadImage(this.animations.walk[0]);
+    }
 
-        this.animate(this.CHICKEN_NORMAL_WALK, 6);
-
-        this.moveLeft(this.speed);
+    update() {
+        this.x -= this.speed;
+        this.playAnimation(this.animations.walk, 8);
     }
 }
