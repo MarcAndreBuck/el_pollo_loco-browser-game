e4a907d2ff5c0b5481854aaf4ebfa6ceb5a3e6e9
Marc-Andre Buck <marcbuck94@gmail.com>
2025-11-22 21:24:09 +0100

    feat(movable-object): implement gravity system and add horizontal movement helpers

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -10,14 +10,21 @@ class MovableObject {
     speed = 0.25;
     otherDirection = false;
 
+    speedY = 0;
+    acceleration = 0.2;
+    hasGravity = true;
+
+    groundY = 430;
+    isGrounded = false;
+
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
     loadImages(arr) {
-        arr.forEach(path => {
-            let img = new Image();
+        arr.forEach((path) => {
+            const img = new Image();
             img.src = path;
             this.imageCache[path] = img;
         });
@@ -33,21 +40,51 @@ class MovableObject {
 
         if (now - this.lastFrameTime >= frameDuration) {
             this.currentImage = (this.currentImage + 1) % images.length;
-
             const path = images[this.currentImage];
             this.img = this.imageCache[path];
-
             this.lastFrameTime = now;
         }
     }
 
     preloadAnimations(animations) {
-        Object.values(animations).forEach(frames => {
+        Object.values(animations).forEach((frames) => {
             this.loadImages(frames);
         });
     }
 
     get worldWidth() {
-    return CONFIG.world.width;
-  }
+        return CONFIG.world.width;
+    }
+
+    get bottom() {
+        return this.y + this.height;
+    }
+
+
+    applyGravity() {
+        if (!this.hasGravity) return;
+
+        this.speedY += this.acceleration;
+        this.y += this.speedY;
+
+        if (this.bottom >= this.groundY) {
+            this.y = this.groundY - this.height;
+            this.speedY = 0;
+            this.isGrounded = true;
+        } else {
+            this.isGrounded = false;
+        }
+    }
+
+    moveHorizontal(dx) {
+        this.x += dx;
+    }
+
+    moveRight(speed = this.speed) {
+        this.moveHorizontal(speed);
+    }
+
+    moveLeft(speed = this.speed) {
+        this.moveHorizontal(-speed);
+    }
 }
