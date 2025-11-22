d23035787d610148f97bf7bfd7acadd614a432e6
Marc-Andre Buck <marcbuck94@gmail.com>
2025-11-22 21:25:28 +0100

    feat(character): integrate gravity, horizontal movement and jump mechanics

--- models/character.class.js
+++ models/character.class.js
@@ -1,44 +1,53 @@
 class Character extends MovableObject {
     x = 50;
-    y = 50;
-    height = 400;
-    width = 180;
+    y = 0;
+    height = 250;
+    width = 112;
     speed = 3;
-    isMoving = false;
-    isJumping = false;
-    lastActionTime = Date.now();
 
+    isMoving = false;
+    lastActionTime = Date.now();
 
     constructor() {
         super();
 
         this.animations = ASSETS.character;
-
         this.preloadAnimations(this.animations);
-
         this.loadImage(this.animations.idle[0]);
     }
 
-
     checkMovement() {
-    if (keyboard.RIGHT) {
-        this.x += this.speed;
-        this.isMoving = true;
-        this.otherDirection = false; 
-        this.resetIdleTimer();
-        return;
+        if (keyboard.RIGHT) {
+            this.moveRight(this.speed)
+            this.otherDirection = false;
+            this.isMoving = true;
+            this.resetIdleTimer();
+            return;
+        }
+
+        if (keyboard.LEFT) {
+            this.moveLeft(this.speed)
+            this.otherDirection = true;
+            this.isMoving = true;
+            this.resetIdleTimer();
+            return;
+        }
+
+        this.isMoving = false;
     }
 
-    if (keyboard.LEFT) {
-        this.x -= this.speed;
-        this.isMoving = true;
-        this.otherDirection = true;   
-        this.resetIdleTimer();
-        return;
+    handleJumpInput() {
+        if (keyboard.SPACE) {
+            this.jump();
+        }
     }
 
-    this.isMoving = false;
-}
+    jump() {
+        if (!this.isGrounded) return;
+        this.speedY = -8;
+        this.isGrounded = false;
+        this.resetIdleTimer();
+    }
 
     resetIdleTimer() {
         this.lastActionTime = Date.now();
@@ -48,30 +57,74 @@ class Character extends MovableObject {
         return Date.now() - this.lastActionTime > 4000;
     }
 
-
-
     updateAnimation() {
-        if (this.isJumping && this.animations.jump) {
-            this.playAnimation(this.animations.jump, 12);  
+        if (!this.isGrounded && this.animations.jump) {
+            this.updateJumpFrame();
             return;
         }
 
         if (this.isMoving && this.animations.walk) {
-            this.playAnimation(this.animations.walk, 12); 
+            this.playAnimation(this.animations.walk, 12);
             return;
         }
 
         if (this.idleTooLong() && this.animations.long_idle) {
-            this.playAnimation(this.animations.long_idle, 8); 
+            this.playAnimation(this.animations.long_idle, 8);
             return;
         }
 
-        this.playAnimation(this.animations.idle, 8); 
+        this.playAnimation(this.animations.idle, 8);
     }
 
     update() {
         this.checkMovement();
+        this.handleJumpInput();
+        this.wasGrounded = this.isGrounded;
+        this.applyGravity();
         this.updateAnimation();
     }
 
+
+    updateJumpFrame() {
+        const frames = this.animations.jump;
+        const v = this.speedY;
+        const d = this.distanceToGround;
+        const margin = 10;
+        let idx;
+
+
+        if (d > 0 && d <= margin && v > 0 && frames.length >= 9) {
+
+            if (d > margin * (2 / 3)) {
+                idx = 6;
+            } else if (d > margin * (1 / 3)) {
+                idx = 7
+            } else {
+                idx = 8
+            }
+
+            this.img = this.imageCache[frames[idx]];
+            return;
+        }
+
+
+
+
+        if (v < -15) {
+            idx = 0;
+        } else if (v < -12) {
+            idx = 1;
+        } else if (v < -10) {
+            idx = 2;
+        } else if (v < 0) {
+            idx = 3;
+        } else if (v < 4) {
+            idx = 4;
+        } else {
+            idx = 5;
+        }
+
+        const path = frames[idx];
+        this.img = this.imageCache[path];
+    }
 }
