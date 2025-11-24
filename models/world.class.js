c5bfc7bf4bbd279f3faa4feba11f3b69a9964bbe
Marc-Andre Buck <marcbuck94@gmail.com>
2025-11-24 14:09:07 +0100

    refactor(world): restructure update and render flow with endscreen and debug

--- models/world.class.js
+++ models/world.class.js
@@ -1,35 +1,3 @@
-const DEBUG_HITBOXES = true;
-
-const HITBOX_VISIBILITY = {
-    player: true,
-    enemy: true,
-    boss: true,
-    collectable: false,
-    projectile: true,
-    other: false,
-};
-
-const HITBOX_COLORS = {
-    player: "#2196f3",
-    enemy: "#f44336",
-    boss: "#9c27b0",
-    collectable: "#4caf50",
-    projectile: "#ffeb3b",
-    other: "#ff9800",
-};
-
-function getObjectCategory(gameObject) {
-    return gameObject?.collisionCategory || "other";
-}
-
-function isHitboxVisibleFor(gameObject) {
-    return !!HITBOX_VISIBILITY[getObjectCategory(gameObject)];
-}
-
-function getHitboxColorFor(gameObject) {
-    return HITBOX_COLORS[getObjectCategory(gameObject)] || HITBOX_COLORS.other;
-}
-
 class World {
     character;
     ctx;
@@ -37,17 +5,25 @@ class World {
     keyboard;
     camera_x = 0;
     worldWidth;
-
+    endScreenImage = null;
+    hasWon = false;
+    gameOver = false;
+    endLoader;
+    collisionSystem;
+    debug;
 
     constructor(canvas, keyboard, level) {
         this.ctx = canvas.getContext("2d");
         this.canvas = canvas;
         this.keyboard = keyboard;
-
-        this.character = new Character();
         this.level = level;
         this.worldWidth = CONFIG.world.width;
 
+        this.character = new Character();
+        this.collisionSystem = new CollisionSystem(this);
+        this.debug = new DebugSystem(this);
+
+        this.initEndscreenLoader();
         this.gameLoop();
     }
 
@@ -74,7 +50,19 @@ class World {
     }
 
     update() {
+        if (this.gameOver || this.hasWon) return;
+
         this.character.update();
+
+        if (this.character.isDead) {
+            this.updateCamera();
+
+            if (this.character.deathFinished) {
+                this.gameOver = true;
+            }
+            return;
+        }
+
         this.keepCharacterInBounds();
         this.updateCamera();
 
@@ -82,7 +70,7 @@ class World {
         this.clouds.forEach(c => c.update && c.update());
         this.collectables.forEach(c => c.update && c.update());
 
-        this.checkCollisions();
+        this.collisionSystem.update();
     }
 
     draw() {
@@ -94,9 +82,17 @@ class World {
         this.addObjectToMap(this.enemies);
         this.addToMap(this.character);
 
-        if (DEBUG_HITBOXES) {
-            this.drawHitboxes();
+        if (this.gameOver) {
+            this.drawEndScreen(ASSETS.start_and_end_screen.game_over);
+            return;
         }
+
+        if (this.hasWon) {
+            this.drawEndScreen(ASSETS.start_and_end_screen.win);
+            return;
+        }
+
+        this.debug.drawHitboxes();
     }
 
     addObjectToMap(objects) {
@@ -104,10 +100,8 @@ class World {
     }
 
     addToMap(mo) {
-        if (!mo.img) return;
 
         const drawX = mo.x - this.camera_x;
-
         this.ctx.save();
 
         if (mo.otherDirection) {
@@ -117,7 +111,7 @@ class World {
                 -drawX - mo.width,
                 mo.y,
                 mo.width,
-                mo.height
+                mo.height,
             );
         } else {
             this.ctx.drawImage(
@@ -125,116 +119,54 @@ class World {
                 drawX,
                 mo.y,
                 mo.width,
-                mo.height
+                mo.height,
             );
         }
 
         this.ctx.restore();
     }
 
-
-    drawHitboxes() {
-        this.drawHitboxFor(this.character);
-        this.enemies.forEach(enemy => this.drawHitboxFor(enemy));
-        this.collectables.forEach(item => this.drawHitboxFor(item));
-    }
-
-    drawHitboxFor(gameObject) {
-        if (!isHitboxVisibleFor(gameObject)) return;
-
-        const hitbox = gameObject.getHitbox();
-        const drawX = hitbox.x - this.camera_x;
-        const color = getHitboxColorFor(gameObject);
-
-        this.ctx.save();
-        this.ctx.beginPath();
-        this.ctx.rect(drawX, hitbox.y, hitbox.width, hitbox.height);
-        this.ctx.lineWidth = 2;
-        this.ctx.strokeStyle = color;
-        this.ctx.stroke();
-        this.ctx.restore();
-    }
-
-
-    areObjectsColliding(objectA, objectB) {
-        const hitboxA = objectA.getHitbox();
-        const hitboxB = objectB.getHitbox();
-
-        const overlapsHorizontally =
-            hitboxA.x < hitboxB.x + hitboxB.width &&
-            hitboxA.x + hitboxA.width > hitboxB.x;
-
-        const overlapsVertically =
-            hitboxA.y < hitboxB.y + hitboxB.height &&
-            hitboxA.y + hitboxA.height > hitboxB.y;
-
-        return overlapsHorizontally && overlapsVertically;
-    }
-
-    checkCollisions() {
-        this.enemies.forEach(enemy => {
-            if (!this.areObjectsColliding(this.character, enemy)) {
-                return;
-            }
-
-            if (this.isStompFromAbove(this.character, enemy)) {
-                console.log("Stomp: Character trifft Enemy von oben:", enemy);
-                if (!enemy.isDead && typeof enemy.die === "function") {
-                    enemy.die();
-                }
-                this.character.speedY = -2;
-                return;
-            }
-
-            console.log("Character bekommt Schaden vom Enemy:", enemy);
-            this.character.takeDamage(1);
-        });
-
-        this.collectables.forEach(item => {
-            if (this.areObjectsColliding(this.character, item)) {
-                console.log("Character collected item:", item);
-                // TODO: Einsammeln, Score, etc.
-            }
-        });
-    }
-
+    /* ---------- Grenzen & Kamera ---------- */
 
     keepCharacterInBounds() {
-        const rightBoundary = this.worldWidth - this.character.width;
-
-        if (this.character.x < 0) {
-            this.character.x = 0;
-        }
-
-        if (this.character.x > rightBoundary) {
-            this.character.x = rightBoundary;
-        }
+        const right = this.worldWidth - this.character.width;
+        this.character.x = Math.max(0, Math.min(this.character.x, right));
     }
 
     updateCamera() {
-        const screenOffsetX = (this.canvas.width - this.character.width) / 2;
-
-        this.camera_x = this.character.x - screenOffsetX;
-
-        if (this.camera_x < 0) {
-            this.camera_x = 0;
-        }
-
+        const centerOffset = (this.canvas.width - this.character.width) / 2;
+        const target = this.character.x - centerOffset;
         const maxCameraX = this.worldWidth - this.canvas.width;
-        if (this.camera_x > maxCameraX) {
-            this.camera_x = maxCameraX;
-        }
+
+        this.camera_x = Math.max(0, Math.min(target, maxCameraX));
     }
 
+    /* ---------- Endscreen ---------- */
 
-    isStompFromAbove(player, enemy) {
-        const playerHitbox = player.getHitbox();
-        const enemyHitbox = enemy.getHitbox();
+    initEndscreenLoader() {
+        this.endLoader = new MovableObject();
+        this.endLoader.loadImages([
+            ...ASSETS.start_and_end_screen.game_over,
+            ...ASSETS.start_and_end_screen.win,
+        ]);
+    }
 
-        const playerBottom = playerHitbox.y + playerHitbox.height;
-        const enemyMiddleY = enemyHitbox.y + enemyHitbox.height * 0.5;
-        const playerIsFalling = player.speedY > 0;
+    drawEndScreen(imageList) {
+        this.endScreenImage ??= this.endLoader.imageCache[
+            imageList[Math.floor(Math.random() * imageList.length)]
+        ];
 
-        return playerIsFalling && playerBottom <= enemyMiddleY;
+        this.ctx.save();
+        this.ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.restore();
+
+        this.ctx.drawImage(
+            this.endScreenImage,
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height,
+        );
     }
 }
