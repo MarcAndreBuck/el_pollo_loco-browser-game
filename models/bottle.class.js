cdfa086c371e1f2553353f1e39874e5898b43d88
Marc-Andre Buck <marcbuck94@gmail.com>
2025-11-25 11:04:56 +0100

    feat(collectables): convert bottles to collectables with random sprite selection
    Updated Bottle class to inherit from Collectable and removed redundant logic.
    Implemented random image selection from bottle sprite array for visual variety.
    Ensured bottles follow same collection handling as coins.

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,15 +1,45 @@
-class Bottle extends MovableObject {
+class Bottle extends Collectable {
+    collisionCategory = "collectable";
+
+    constructor(x, y) {
+        super(x, y, 60, 60);
+
+        this.animations = ASSETS.bottle;
+        this.snapToGround();
+
+        this.loadImage(this.animations.on_ground[Math.floor(Math.random() * 2)]);
+
+        this.value = 1;
+        this.setHitbox(20, 10, 30, 40);
+    }
+
+     update() {
+    }
+
+    onCollect(world) {
+        world.bottle = (world.bottle) + this.value;
+        super.onCollect(world);
+    }
+}
+
+
+
+class ThrowBottle extends MovableObject {
+    collisionCategory = "projectile";
+
     constructor(x, y) {
         super();
 
-        this.animations = ASSETS.bottle; 
+        this.animations = ASSETS.bottle;
         this.x = x;
-        this.snapToGround() ;
+        this.snapToGround();
 
-        this.loadImage(this.animations.on_ground[0]); 
+        this.loadImage(this.animations.rotation[0]);
+        this.playAnimation(this.animations.rotation)
+        this.setHitbox(0, 95, 100, 100);
     }
 
     update() {
-     
+
     }
 }
