a4f35b33b277291c8b3305df67e86589bf9fadc1
Marc-Andre Buck <marcbuck94@gmail.com>
2025-11-26 13:34:08 +0100

    feat(enemies): add small chicken enemy type

--- models/enemies.class.js
+++ models/enemies.class.js
@@ -40,3 +40,47 @@ class Chicken extends MovableObject {
         else this.playAnimation(this.animations.walk, 8);;
     }
 }
+
+
+class SmallChicken extends MovableObject {
+    collisionCategory = "enemy";
+
+    constructor() {
+        super();
+
+        this.animations = ASSETS.chicken_small;
+        this.feetOffset = -10;
+
+        this.speed = 0.2 + Math.random() * 0.4;
+
+        this.health = 1;
+        this.isDead = false;
+        this.deathFinished = false;
+
+        this.x = 200 + Math.random() * 500;
+        this.snapToGround();
+
+        this.preloadAnimations(this.animations);
+        this.loadImage(this.animations.walk[0]);
+        this.setHitbox(0, 0, 100, 100);
+    }
+
+    update() {
+
+        this.updateAnimation();
+        this.applyGravity();
+
+        if (!this.isDead) {
+            this.moveLeft(this.speed);
+        }
+    }
+
+    updateAnimation() {
+        
+        if (this.isDead) {
+            this.playAnimation(this.animations.dead, 8, false);
+            return
+        }
+        else this.playAnimation(this.animations.walk, 8);;
+    }
+}
