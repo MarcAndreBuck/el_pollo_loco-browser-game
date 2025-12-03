347503d002bdb6b761da9dd21b793716dcd0b420
Marc-Andre Buck <marcbuck94@gmail.com>
2025-12-03 15:06:16 +0100

    refactor(audio): optimize sound.js to reduce loading times

--- js/sound.js
+++ js/sound.js
@@ -2,31 +2,24 @@
 
 const SOUNDS = {
 
-    /* ---------------- MUSIC ---------------- */
 
-    // music_menu: {
-    //     src: "sounds/music/bgm-menu.mp3",        // short-break-363190
-    //     loop: true,
-    //     volume: 0.5,
-    //     category: "music",
-    // },
 
     music_level: {
-        src: "sounds/freepik__vibrant-shadows-of-the-fiesta__1131.mp3",    // vibrant-shadows-of-the-fiesta
+        src: "sounds/freepik__vibrant-shadows-of-the-fiesta__1131.mp3",
         loop: true,
         volume: 0.2,
         category: "music",
     },
 
     music_boss: {
-        src: "sounds/freepik__fiesta-fury_-battle-for-the-crown__1128.mp3",     // fiesta-fury_-battle-for-the-crown
+        src: "sounds/freepik__fiesta-fury_-battle-for-the-crown__1128.mp3",
         loop: true,
         volume: 0.6,
         category: "music",
     },
 
 
-    /* ---------------- BOSS ---------------- */
+
 
     boss_hurt: {
         src: "sounds/chicken-hurt.wav",
@@ -36,28 +29,28 @@ const SOUNDS = {
     },
 
     boss_death: {
-        src: "sounds/chicken-death.wav", 
+        src: "sounds/chicken-death.wav",
         loop: false,
         volume: 1.0,
         category: "sfx",
     },
 
     boss_idle: {
-        src: "sounds/chicken-idle.wav", 
+        src: "sounds/chicken-idle.wav",
         loop: true,
         volume: 0.3,
         category: "sfx",
     },
 
     boss_alert: {
-        src: "sounds/chicken-alert.wav", 
+        src: "sounds/chicken-alert.wav",
         loop: false,
         volume: 0.6,
         category: "sfx",
     },
 
     boss_attack: {
-        src: "sounds/chicken-attack.wav", 
+        src: "sounds/chicken-attack.wav",
         loop: false,
         volume: 0.7,
         category: "sfx",
@@ -67,7 +60,7 @@ const SOUNDS = {
     /* ---------------- CHICKEN ---------------- */
 
     chicken_death: {
-        src: "sounds/chicken-430403.mp3", 
+        src: "sounds/chicken-430403.mp3",
         loop: false,
         volume: 0.9,
         category: "sfx",
@@ -83,7 +76,7 @@ const SOUNDS = {
     },
 
     player_step: {
-        src: "sounds/sand_step-87182 (mp3cut.net).wav", 
+        src: "sounds/sand_step-87182 (mp3cut.net).wav",
         loop: false,
         volume: 0.5,
         category: "sfx",
@@ -93,20 +86,6 @@ const SOUNDS = {
         src: "sounds/ouchmp3-14591.mp3",
         loop: false,
         volume: 0.9,
-        category: "sfx",
-    },
-
-    player_land: {
-        src: "sounds/land2-43790.mp3",
-        loop: false,
-        volume: 0.6,
-        category: "sfx",
-    },
-
-    player_throw: {
-        src: "sounds/knife-throw-1-105221.mp3",
-        loop: false,
-        volume: 0.7,
         category: "sfx",
     },
 
@@ -124,17 +103,10 @@ const SOUNDS = {
         category: "sfx",
     },
 
-     player_snore: {
+    player_snore: {
         src: "sounds/big-snore-78224 (mp3cut.net).wav",
         loop: false,
         volume: 0.9,
-        category: "sfx",
-    },
-
-    bottle_fly: {
-        src: "sounds/rotate-movement-whoosh-3-185337 (mp3cut.net).wav",
-        loop: false,
-        volume: 0.6,
         category: "sfx",
     },
 
